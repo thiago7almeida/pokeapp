2be502172df98b065fe1a2c893e52e4fbf15a5f3
thiago7almeida <thiagoa.silva@outlook.com>
2023-04-09 10:30:26 -0300

    chore: configured alias paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@components': './packages/components',
+          '@main': './packages/main',
+        },
+      },
+    ],
+  ],
 };
